e7647d5259059899b7501e0310c674469aeaa0ff
unknown <45726273+zeyad-mansour@users.noreply.github.com>
2021-04-17 01:21:58 -0400

    commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Lunar 🌕
-Lunar is a neural network aimbot that uses real-time object detection; CUDA acceleration on Nvidia GPUs allows for extremely fast object detection.
+Lunar is a neural network aimbot that uses real-time object detection aided by CUDA acceleration on Nvidia GPUs.
+
+## About
+
+Lunar can be modified to work with a variety of FPS games; however, it is currently configured for Fortnite. Besides being easy-to-use, the main benefit of Lunar is that it is virtually undetectable by anti-cheat software (no memory is meddled with).
+
+The basis of Lunar's player detection is the [YOLOv5](https://github.com/ultralytics/yolov5) architecture written in the PyTorch framework.
+
+## Installation
+
+1. Install [Python](https://www.python.org/downloads/) 3.8 or later
+
+
+2. Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the necessary dependencies.
+
+```
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+```
+python lunar.py
+```
+## Future Developments
+- Train a custom model to detect players with a greater mAP than the YOLOv5s default person detection.
+- Explore combining real-time object detection with pixel color tracking
+- Smooth, natural player tracking
+
+
+## Contributing
+Pull requests, suggestions, or issue/bug reports are welcome. If you have any of those, please open an [issue](https://github.com/zeyad-mansour/Lunar/issues) and provide some detail.
--- lib/aimbot.py
+++ lib/aimbot.py
@@ -11,11 +11,12 @@ import signal
 import sys
 import torch
 import win32api
-from ctypes import windll
-import ctypes
 import pyautogui
+import subprocess
+import ctypes
 
 PUL = ctypes.POINTER(ctypes.c_ulong)
+
 class KeyBdInput(ctypes.Structure):
     _fields_ = [("wVk", ctypes.c_ushort),
                 ("wScan", ctypes.c_ushort),
@@ -23,28 +24,30 @@ class KeyBdInput(ctypes.Structure):
                 ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
 
+
 class HardwareInput(ctypes.Structure):
     _fields_ = [("uMsg", ctypes.c_ulong),
                 ("wParamL", ctypes.c_short),
                 ("wParamH", ctypes.c_ushort)]
+
 
 class MouseInput(ctypes.Structure):
     _fields_ = [("dx", ctypes.c_long),
                 ("dy", ctypes.c_long),
                 ("mouseData", ctypes.c_ulong),
                 ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
+
 
 class Input_I(ctypes.Union):
     _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
 
 class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
-
 
 class Aimbot:
     def __init__(self):
@@ -54,23 +57,27 @@ class Aimbot:
         self.half_screen_width = self.screen.monitors[1]["width"] / 2
         self.half_screen_height = self.screen.monitors[1]["height"] / 2
 
-        #this controls the initial box width and height of the Lunar Vision window
+        #this controls the initial box width and height of the "Lunar Vision" window
+        #you can modify this to change how large you want the detection area to be
         self.centered_box_constant = 300
 
         #You can change these offsets
-        self.detection_box = (int(self.half_screen_width - self.centered_box_constant/2 + 50),  #x coord
-                              int(self.half_screen_height - self.centered_box_constant/2 - 50), #y coord
-                              int(self.half_screen_width + self.centered_box_constant/2 ),  #width of the box
-                              int(self.half_screen_height + self.centered_box_constant/2)) #height of the box
+        self.detection_box = (int(self.half_screen_width - self.centered_box_constant/16),  #x1 coord
+                              int(self.half_screen_height - self.centered_box_constant/2), #y1 coord
+                              int(self.half_screen_width + self.centered_box_constant/2),  #x2 coord
+                              int(self.half_screen_height + self.centered_box_constant/2)) #y2 coord
         print("[INFO] Loading the neural network model")
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s6')
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload = True)
         if torch.cuda.is_available():
             print(colored("CUDA ACCELERATION [ENABLED]", "green"))
         else:
             print(colored("[!] CUDA ACCELERATION IS UNAVAILABLE", "red"))
             print(colored("[!] Check your pytorch installation, else performance will be very poor", "red"))
-        self.model.conf = 0.45  # confidence threshold (0-1)
-        self.model.iou = 0.45  # NMS IoU threshold (0-1)
+
+        self.model.conf = 0.35   # confidence threshold (or base detection (0-1)
+
+        self.model.iou = 0.45 # NMS IoU (0-1)
+
         self.model.classes = [0] #only include the person class
         time.sleep(1)
         print("\n[INFO] PRESS 'F1' TO TOGGLE AIMBOT\n[INFO] PRESS 'F2' TO QUIT")
@@ -89,19 +96,23 @@ class Aimbot:
         y = int(y - self.half_screen_height)
         extra = ctypes.c_ulong(0)
         ii_ = Input_I()
+        ii_.mi.time = 0
         ii_.mi = MouseInput(x, y, 0, 0x0001, 0, ctypes.pointer(extra))
         command = Input(ctypes.c_ulong(0), ii_)
         ctypes.windll.user32.SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
+
 
     def start(self):
         print("[INFO] Beginning screen capture")
 
         Aimbot.update_status_aimbot(self)
+        dc = ctypes.windll.user32.GetDC(0)
 
         while True:
             last_time = time.time()
 
             frame = np.array(self.screen.grab(self.detection_box))
+            cv2.rectangle(frame, (0, self.centered_box_constant//2), (self.centered_box_constant//16, self.centered_box_constant * 2), (0, 0, 0), -1) #draw box over own player
             results = self.model(frame)
 
             if len(results.xyxy[0]) != 0: #person detected
@@ -109,7 +120,7 @@ class Aimbot:
                 for *box, conf, cls in results.xyxy[0]:
                     x1y1 = tuple(box[:2])
                     x2y2 = tuple(box[2:])
-                    cv2.rectangle(frame, x1y1, x2y2, color=[255, 255, 255], thickness=2) #draw the bounding boxes
+                    cv2.rectangle(frame, x1y1, x2y2, color=[0, 0, 255], thickness=2) #draw the bounding boxes
                     cv2.putText(frame, f"{int(conf * 100)}%", x1y1,cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2) #draw the confidence labels on the bounding boxes
 
                     # object detections are automatically ordered from greatest to least confidence
@@ -121,15 +132,19 @@ class Aimbot:
                 #get proper coordinates and move the mouse if aimbot enabled
                 if self.aimbot_status == colored("ENABLED", 'green'):
                     height = y2 - y1
-                    headX, headY = int((x1 + x2)/2), int((y1 + y2)/2 - height/3) #offset to roughly approximate the head using a ratio of the height
+                    headX, headY = int((x1 + x2)/2), int((y1 + y2)/2 - height/2.5) #offset to roughly approximate the head using a ratio of the height
 
-                    cv2.circle(frame, (headX, headY), 5, (0, 0, 255), -1)
+                    cv2.circle(frame, (headX, headY), 6, (0, 255, 0), -1)
 
                     absX, absY = headX + self.detection_box[0], headY + self.detection_box[1]
-                    Aimbot.move_crosshair(self, absX, absY)
+
+                    #checks the pixel val to see if the pickaxe is not selected
+                    #this pixel val is Fortnite specific to 1920x1080 resolution at 75% HUD; it would need to be modified for any other configuration
+                    if ctypes.windll.gdi32.GetPixel(dc, 1563, 953) != 16777215: #checks the pixel val to see if the pickaxe is not selected
+                        Aimbot.move_crosshair(self, absX, absY)
 
 
-            cv2.putText(frame, f"FPS {1 // (time.time() - last_time)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(frame, f"FPS: {int(1 / (time.time() - last_time))}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv2.imshow("Lunar Vision", frame)
 
             if cv2.waitKey(1) & 0xFF == ord('0'):
